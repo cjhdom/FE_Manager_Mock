5a93190a3aec4eea8b68bcd4c875a4a52aa840fd
jihwchoi <jihwchoi@ebay.com>
2017-01-19 17:37:33 +0900

    z

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,32 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/getWeightAll', getWeightAll);
+router.put('/setWeightList', setWeightList);
+
+function getWeightAll (req, res) {
+
+}
+
+function setWeightList (req, res) {
+  var backend = req.query['backend'];
+  var servers = req.query['servers'];
+
+  try {
+    servers = JSON.parse(servers);
+  } catch (e) {
+    return res.json(500, false);
+  }
+
+  async.each(servers, function (server, callbackEach) {
+
+  }, function (error) {
+    if (error) {
+      res.json(500, false);
+    } else {
+      res.json('Success');
+    }
+  });
+}
+
 module.exports = router;
