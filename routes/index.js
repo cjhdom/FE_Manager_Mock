db03e252fd782e0612921d85e9a5e30e5cffff61
jihwan <ferus_gm@hanmail.net>
2017-01-19 22:49:08 +0900

    test

--- routes/index.js
+++ routes/index.js
@@ -10,14 +10,26 @@ router.get('/getWeightAll', getWeightAll);
 router.put('/setWeightList', setWeightList);
 
 function getWeightAll (req, res) {
-
+  res.json('hello');
 }
 
 function setWeightList (req, res) {
-  var backend = req.query['backend'];
+  //var backend = req.query['backend'];
   var servers = req.query['servers'];
-
+  var serversParsed = null;
   try {
+    servers = JSON.parse(servers);
+  } catch (e) {
+    return res.json(typeof servers);
+  }
+
+  res.json('typeof servers ' + typeof servers + ' typeof parsed' + typeof serversParsed);
+
+
+
+
+
+  /*try {
     servers = JSON.parse(servers);
   } catch (e) {
     return res.json(500, false);
@@ -31,7 +43,7 @@ function setWeightList (req, res) {
     } else {
       res.json('Success');
     }
-  });
+  });*/
 }
 
 module.exports = router;
